4939da56cfe93da5263191947b4ae0da04edb116
Dave Dopson <ddopson@zynga.com>
2011-11-28 13:57:22 -0800

    tabs ==> spaces

--- example.js
+++ example.js
@@ -1,65 +1,65 @@
-var sys      = require('sys');
+var sys    = require('sys');
 var Memcache = require('./lib/memcache');
 
 function microtime(get_as_float) {  
-    var now = new Date().getTime() / 1000;  
-    var s = parseInt(now);
-    return (get_as_float) ? now : (Math.round((now - s) * 1000) / 1000) + ' ' + s;  
+  var now = new Date().getTime() / 1000;  
+  var s = parseInt(now);
+  return (get_as_float) ? now : (Math.round((now - s) * 1000) / 1000) + ' ' + s;  
 }
 
 var onConnect = function() {
-	mcClient.get('test', function(err, data) {
-		sys.debug(data);
-		mcClient.close();
-	});
+  mcClient.get('test', function(err, data) {
+    sys.debug(data);
+    mcClient.close();
+  });
 };
 
 var benchmark = function() {
-	var count = 20000;
-	start = microtime(true);
-	var x = 0;
-	
-	for (var i=0; i<=count; i++) {
-		mcClient.get('test', function(err, data) {
-			x += 1;
-			if (x == count) {
-				end = microtime(true);
-				sys.debug('total time: ' + (end - start));
-			}
-		});
-	}
-	
-	mcClient.close();
+  var count = 20000;
+  start = microtime(true);
+  var x = 0;
+  
+  for (var i=0; i<=count; i++) {
+    mcClient.get('test', function(err, data) {
+      x += 1;
+      if (x == count) {
+        end = microtime(true);
+        sys.debug('total time: ' + (end - start));
+      }
+    });
+  }
+  
+  mcClient.close();
 };
 
 var setKey = function() {
-	mcClient.set('test', 'hello \r\n node-memcache', function(err, response) {
-		mcClient.get('test', function(err, data) {
-			sys.debug(data);
-			mcClient.close();
-		});
-	});
+  mcClient.set('test', 'hello \r\n node-memcache', function(err, response) {
+    mcClient.get('test', function(err, data) {
+      sys.debug(data);
+      mcClient.close();
+    });
+  });
 };
 
 var version = function() {
-	mcClient.version(function(err, version) {
-		sys.debug(version);
-		mcClient.close();
-	});
+  mcClient.version(function(err, version) {
+    sys.debug(version);
+    mcClient.close();
+  });
 };
 
 var incr = function() {
-	mcClient.increment('x', 2, function(err, new_value) {
-		sys.debug(new_value);
-		mcClient.close();
-	});
+  mcClient.increment('x', 2, function(err, new_value) {
+    sys.debug(new_value);
+    mcClient.close();
+  });
 };
 
 var decr = function() {
-	mcClient.decrement('x', 1, function(err, new_value) {
-		sys.debug(new_value);
-		mcClient.close();
-	});
+  mcClient.decrement('x', 1, function(err, new_value) {
+    sys.debug(new_value);
+    mcClient.close();
+  });
 };
 
 mcClient = new Memcache();
